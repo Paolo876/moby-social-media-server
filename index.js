41d4e35c9ad8d6aa8afd3753151add6ff19981ca
paoloBugarin <paolopaolobugarin@gmail.com>
2023-01-29 17:21:10 -0800

    created chat routes

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ app.get("/", (req,res) => res.send("APP IS ONLINE..."))
 app.use("/api/auth", require("./routes/Users"));
 app.use("/api/posts", require("./routes/Posts"));
 app.use("/api/comments", require("./routes/Comments"));
+app.use("/api/chat", require("./routes/Chat"));
 app.use("/api/imagekit", require("./routes/Imagekit"));
 
 
