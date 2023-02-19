8e7641c5f9aaee11a3840cc7868977ec2c962218
paoloBugarin <paolopaolobugarin@gmail.com>
2023-02-19 13:48:38 -0800

    created parseCookie utility function, updated auth events on socketio

--- events/index.js
+++ events/index.js
@@ -1,11 +1,35 @@
 
+const jwt = require("jsonwebtoken");
+const parseCookie = require("../utils/parseCookie")
+
 
 const index = (io, socket) => {
-    console.log(socket.handshake.headers.cookie);
+
+    // //authorize token if exists
+    // if(socket.request.headers.cookie){
+    //     console.log(socket.request.headers.cookie)
+    //     const userId = authorizeToken(socket.request.headers.cookie)
+    //     console.log(userId)
+    // }
+
+    //listen to login
+    socket.on('login', () => {
+        // console.log(socket.handshake.headers.cookie)
+        const userId = authorizeToken(socket.request.headers.cookie)
+        console.log("logged in", userId)
+    })
+
     socket.on('disconnect', () => {
       console.log('user disconnected');
     });
 
+
+}
+
+const authorizeToken = (cookie) => {
+    const token = parseCookie(cookie).token
+    const user = jwt.verify(token, process.env.JWT_SECRET);    
+    return user.id;
 }
 
 module.exports = index;
