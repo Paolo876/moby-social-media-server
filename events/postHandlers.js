614100dd82d732a8d3c94c6275f3fbba95b3dea3
paoloBugarin <paolopaolobugarin@gmail.com>
2023-03-05 22:02:04 -0800

    updated postHandlers

--- events/postHandlers.js
+++ events/postHandlers.js
@@ -7,9 +7,8 @@ const postHandlers = async (socket, UserId) => {
     /* @desc  emit created post to friends
     */
     socket.on('emit-created-post', async (data) => {
-        console.log(data)
         const friendSockets = await checkOnlineFriends(UserId)   // [{socket}]
-        friendSockets.forEach(item => socket.to(item.socket).emit("receive-created-post", data.requestData))
+        friendSockets.forEach(item => socket.to(item.socket).emit("receive-created-post", data))
     });
 
     /* @desc  emit comment to post author
