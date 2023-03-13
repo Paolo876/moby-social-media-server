f53da2ac7827fb366fca8c4eea38805760a622f6
paoloBugarin <paolopaolobugarin@gmail.com>
2023-03-13 00:16:18 -0700

    updated database file

--- config/database.js
+++ config/database.js
@@ -1,15 +1,24 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_URL, { 
-    dialect: "mysql", 
-    logging: false, 
-});
-// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { 
-//     host: process.env.DB_HOST, 
+// const sequelize = new Sequelize(process.env.DB_URL, { 
 //     dialect: "mysql", 
 //     logging: false, 
-//     port: process.env.DB_PORT 
 // });
+
+let sequelize;
+if(process.env.NODE_ENV === "local"){
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, { 
+        host: process.env.DB_HOST, 
+        dialect: "mysql", 
+        logging: false, 
+        port: process.env.DB_PORT 
+    });
+} else {
+  sequelize = new Sequelize(process.env.DB_URL, { 
+        dialect: "mysql", 
+        logging: false, 
+    });
+}
 
 
 const testDBConnection = async () => {
